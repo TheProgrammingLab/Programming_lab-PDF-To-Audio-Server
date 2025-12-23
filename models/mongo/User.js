ff225af96910e35856d2b38bb89fb1c1a696a048
Kareem Taiye <olayinkahassan117@gmail.com>
2025-12-23 10:43:16 +0000

    fixed signup issue

--- models/mongo/User.js
+++ models/mongo/User.js
@@ -4,16 +4,16 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Missing email"],
-    trim: true,
     minlength: [1, "email cannot be empty"],
-    unique: true,
     trim: true,
+    unique: true,
   },
   password: {
     type: String,
     required: [true, "Missing password"],
     trim: true,
     minlength: [1, "password cannot be empty"],
+    select: false,
   },
 });
 
